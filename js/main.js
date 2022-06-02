b4d1d4dcca82758735f1cf3db18d6b85be162ddc
Mark Mac <markmac.web@gmail.com>
2022-06-02 21:30:45 +0100

    created buttons for numpad, layout built with grid. Created main.js file and setup initial querySelectors

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const playerScore = document.querySelector("#playerMainCount")
+
+const buttons = document.querySelectorAll("button")
+
+const currentTurn = document.querySelector("#currentTurnScore")
+
