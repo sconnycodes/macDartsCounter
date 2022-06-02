752baa62b89ccb58172e661222a95cceae1aa200
Mark Mac <markmac.web@gmail.com>
2022-06-02 21:51:42 +0100

    linked main.js to html, button clicks print value to console, query selectors working

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 const playerScore = document.querySelector("#playerMainCount")
 
-const buttons = document.querySelectorAll("button")
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(item => {
+    item.addEventListener("click", event => {
+    const {target} = event;
+    const {value} = target;
+    
+    if (!target.matches("button")){
+        return
+    } else {
+        // calculator.parseInput(value)
+        console.log(value)
+    }
+    })
+})
 
 const currentTurn = document.querySelector("#currentTurnScore")
 
