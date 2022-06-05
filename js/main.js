e52975fa1b4828e8f41e6908046bf21134be5513
Mark Mac <markmac.web@gmail.com>
2022-06-05 02:24:24 +0100

    started js for game, setting up class constructor for games, this will be relevant for when server keeps count to use for  gameID to allow for  stat tracking

--- js/main.js
+++ js/main.js
@@ -1,20 +1,43 @@
-const playerScore = document.querySelector("#playerMainCount")
+//grabbing mainScore display and currentTurn display
+const mainScoreDisplay = document.querySelector("#playerMainCount")
+const currentTurnScoreDisplay = document.querySelector("#currentTurnScore")
 
+
+// Setting up event listeners on buttons.
 const buttons = document.querySelectorAll("button");
-
 buttons.forEach(item => {
     item.addEventListener("click", event => {
     const {target} = event;
     const {value} = target;
     
-    if (!target.matches("button")){
-        return
-    } else {
-        // calculator.parseInput(value)
+    // if (value == "back"){
+    //     return to previous playerScore
+    // } else if (value == "next") {
+    //      deduct current turn score from playerScore
+    // } else {
+        // add value of button to currentScore
+        currentTurnScore.innerText += value
         console.log(value)
-    }
+    // }
     })
-})
+});
 
-const currentTurn = document.querySelector("#currentTurnScore")
 
+// game setup as object
+class Game {
+    constructor(startNumber) {
+        this.mainScore = startNumber || 501,
+        this.currentTurn = 0,
+        this.prevTurn = 0
+    }
+    
+    //function to run when next is clicked
+    next(){
+        //sub currentTurn score from mainScore and update display
+    }
+
+    //function to run when back is clicked
+    back(){
+        //add prev turn score back to main and update display
+    }
+};
